ee7640ee1a74bf696b64905a02167de03619e56e
kevin xirum <kevinmenjivar66@gmail.com>
2020-05-16 19:30:27 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,7 +12,7 @@ end
 
 def assign_rooms(speakers)
   assign = []
-  speakers.each_with_index {|speaker,index}
+  speakers.each_with_index {|speaker,index
   assign.push ("Hello, #{speaker}! You'll be assigned to room #{index+1}!")
  end 
  return room_array
