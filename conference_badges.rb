e0aedc46efd701cea6ee301f7af0a50533ddc58f
kevin xirum <kevinmenjivar66@gmail.com>
2020-05-16 19:36:28 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,7 +4,7 @@ end
 attendees = ["Edsger"]
 
 def batch_badge_creator(attendees)
-  badges = ["Edsger"]
+  badges = []
   attendees.each do |badge|
     badges.push ("Hello, my name is #{name}.")
   end
